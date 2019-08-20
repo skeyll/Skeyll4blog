0a0aff550b7bb4378c230d96256fed99bf6fcd22
SkeyllK <taki.fukuyama@gmail.com>
2019-08-21 08:28:35 +0900

    Add "cmd" and saparete "fin" from rsv...

--- PrincessConnect/PrincessConnect/PrincessConnect.py
+++ PrincessConnect/PrincessConnect/PrincessConnect.py
@@ -18,6 +18,7 @@ Booking3 = []
 Booking4 = []
 Booking5 = []
 
+
 @client.event
 async def on_message(message):
     listFlag = 0
@@ -46,6 +47,8 @@ async def on_message(message):
                if Boss in message.content:
                   BookList = "Booking" + Boss
                   await message.channel.send(eval(BookList))
+           listFlag =  0
+
        elif removeFlag == 1:
            for Boss in BossNum:
                if Boss in message.content:
@@ -53,19 +56,27 @@ async def on_message(message):
                    eval(BookList).remove(message.author.display_name)      
            reply = "Did it! >" + message.author.display_name
            await message.channel.send(reply)
+           removeFlag = 0
+
        elif endFlag == 1:
            for Boss in BossNum:
                   BookList = "Booking" + Boss
                   eval(BookList).clear()
-           reply = "I clear all lists!"
+           reply = "予約を全削除"
            await message.channel.send(reply)
+           endFlag = 0
+
        elif bookFlag == 1:
+           bossCount = 0
            for Boss in BossNum:
                if Boss in message.content:
                   BookList = "Booking" + Boss
                   eval(BookList).append(message.author.display_name)
-                  reply = "Book on " + str(Boss) + "! >" + message.author.display_name
-                  await message.channel.send(reply)
+                  bossCount += 1
+           reply = str(bossCount) + "件の予約 >" + message.author.display_name
+           await message.channel.send(reply)
+           bookFlag = 0
+           
  #      elif mentionFlag == 1:
  #           mentionList = []
  #           for Boss in BossNum:
@@ -75,16 +86,29 @@ async def on_message(message):
  #           for one in eval(BookList):
  #               mentionList.append(" @" + one + " ")
  #           await message.channel.send(mentionList)
+ #           mentionFlag = 0
+
        elif displayFlag == 1: # Display all book list
            for Boss in BossNum:
                BookList = "Booking" + Boss
                await message.channel.send(eval(BookList))
+           displayFlag = 0
 
-       listFlag =  0
-       removeFlag = 0
-       bookFlag = 0
-       endFlag = 0
-       displayFlag = 0
- #      mentionFlag = 0
+    elif message.content.startswith("fin"):
+        for Boss in BossNum:
+               if Boss in message.content:
+                   BookList = "Booking" + Boss
+                   eval(BookList).remove(message.author.display_name)      
+        reply = "削除完了 >" + message.author.display_name
+        await message.channel.send(reply)
+    
+    elif message.content.startswith("cmd"):
+        await message.channel.send("予約:rsv 1-5 / 予約表示:rsv list 1-5 / 予約全表示:rsv! /　予約削除:fin 1-5 / 予約全削除:rsv END")
+       
+       
+ 
+
+
+
 
 client.run(TOKEN)
